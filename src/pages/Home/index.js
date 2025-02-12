5f9ef4d72367695f694da4116ab4cb3d76559961
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2025-02-12 12:09:20 +0500

    remove snow

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer"
 import { Outlet } from "react-router";
-import Snow from "../../components/Snow";
 
 const Home = () => {
     return (
         <div>
-            <Snow />
             <Navbar />
             <Outlet />
             <Footer />
