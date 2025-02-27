2ece291af08db2d8650b3ee1af0a986e5317032a
Nodirbek Nurmamatov <103112982+nodirbek27@users.noreply.github.com>
2025-02-27 13:44:48 +0500

    oquv dasturlari

--- src/components/Curriculum/index.jsx
+++ src/components/Curriculum/index.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 import TextTranslate from "../TextTranslate";
 import { FaRegCalendarAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Curriculum() {
   return (
     <div className="bg-slate-100">
       <div className="max-w-xs md:max-w-7xl mx-auto md:py-16 py-10">
         <h1 className="text-2xl md:text-4xl font-bold text-[#004269]">
-          <TextTranslate id="oquvDasturlari"/>
+          <TextTranslate id="oquvDasturlari" />
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:mx-10 lg:mx-0 mt-12">
           <div className="flex w-full h-[140px] bg-white p-3 rounded-2xl duration-200 hover:scale-105 shadow-md hover:shadow-lg">
             <div className="w-[200px]">
               <img
                 className="rounded-md w-full h-full object-fill"
-                src="https://img.freepik.com/free-vector/hand-drawn-science-background-theme_23-2148538097.jpg?w=1380&t=st=1702876479~exp=1702877079~hmac=dc71321736e991365d2d2ac2ef81b5d2999e33b5d5624438d47a2496282d6a51"
+                src="https://bilimlar.uz/wp-content/uploads/2021/03/books.jpg"
                 alt=""
               />
             </div>
             <div className="pl-3 w-full">
               <h2 className="text-md md:text-xl font-medium text-slate-600">
-                <TextTranslate id="fanlarKatalog"/>
+                <TextTranslate id="fanlarKatalog" />
               </h2>
               <p className="text-sm flex items-center">
                 <span>
                   <FaRegCalendarAlt className="mr-1" />
                 </span>
-                12.12.2023
+                12.01.2025
               </p>
               <div className="text-end">
-                <button className="text-sm bg-[#004269] text-white mt-5 md:mt-4 px-2 py-1 rounded-md">
-                <TextTranslate id="oquvDasturlariBatafsil"/>
-                </button>
+                <Link
+                  to="/fanKatalogi"
+                  className="inline-block text-sm bg-[#004269] text-white mt-5 md:mt-4 px-2 py-1 rounded-md"
+                >
+                  <TextTranslate id="oquvDasturlariBatafsil" />
+                </Link>
               </div>
             </div>
           </div>
@@ -39,24 +43,27 @@ function Curriculum() {
             <div className="w-[200px]">
               <img
                 className="rounded-md w-full h-full object-fill"
-                src="https://static.imtihonlar.uz/crop/1/3/832__85_1367880221.jpg?t=1712574204"
+                src="https://yuz.uz/imageproxy/1200x/https://yuz.uz/file/news/b6e93da1d1303c27c6f7b5d47d2d03d3.jpg"
                 alt=""
               />
             </div>
             <div className="pl-3 w-full">
               <h2 className="text-md md:text-xl font-medium text-slate-600">
-              <TextTranslate id="bakalavrMalakaTalablari"/>
+                <TextTranslate id="bakalavrMalakaTalablari" />
               </h2>
               <p className="text-sm flex items-center">
                 <span>
                   <FaRegCalendarAlt className="mr-1" />
                 </span>
-                12.12.2023
+                12.02.2025
               </p>
               <div className="text-end">
-                <button className="text-sm bg-[#004269] text-white mt-5 md:mt-4 lg:mt-4 px-2 py-1 rounded-md">
-                <TextTranslate id="oquvDasturlariBatafsil"/>
-                </button>
+                <Link
+                  to="/abiturient-xorijiy-talabalar"
+                  className="inline-block text-sm bg-[#004269] text-white mt-5 md:mt-4 lg:mt-4 px-2 py-1 rounded-md"
+                >
+                  <TextTranslate id="oquvDasturlariBatafsil" />
+                </Link>
               </div>
             </div>
           </div>
@@ -64,24 +71,27 @@ function Curriculum() {
             <div className="w-[200px]">
               <img
                 className="rounded-md w-full h-full object-fill"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrQ_bsktOs4EYLUQZ2sw6vj06ND3lciL4f7g&usqp=CAU"
+                src="https://www.uzreport.news/fotobank/image/46ca5cd9e619e47ed9f44b2df801ad6f.jpeg"
                 alt=""
               />
             </div>
             <div className="pl-3 w-full">
               <h2 className="text-md md:text-xl font-medium text-slate-600">
-              <TextTranslate id="magistrMalakaTalablari"/>
+                <TextTranslate id="magistrMalakaTalablari" />
               </h2>
               <p className="text-sm flex items-center">
                 <span>
                   <FaRegCalendarAlt className="mr-1" />
                 </span>
-                12.12.2023
+                12.01.2025
               </p>
               <div className="text-end">
-                <button className="text-sm bg-[#004269] text-white mt-5 md:mt-4 lg:mt-4 px-2 py-1 rounded-md">
-                  <TextTranslate id="oquvDasturlariBatafsil"/>
-                </button>
+                <Link
+                  to="/oquvRejalari"
+                  className="inline-block text-sm bg-[#004269] text-white mt-5 md:mt-4 lg:mt-4 px-2 py-1 rounded-md"
+                >
+                  <TextTranslate id="oquvDasturlariBatafsil" />
+                </Link>
               </div>
             </div>
           </div>
